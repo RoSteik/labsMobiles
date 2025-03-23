e6e09ba67a836dbc54c843baed8662fda099d303
Rostyslav Postolaki <rostyslav.postolaki@gmail.com>
2025-03-23 16:44:49 +0200

    lab_3

--- lib/lab2/pages/user_profile_page.dart
+++ lib/lab2/pages/user_profile_page.dart
@@ -1,36 +1,64 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-
 import 'package:my_project/lab2/elements/responsive_config.dart';
+import 'package:my_project/lab2/logic/model/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
-class UserProfilePage extends StatelessWidget {
+class UserProfilePage extends StatefulWidget {
   const UserProfilePage({super.key});
+
+  @override
+  State<UserProfilePage> createState() => _UserProfilePageState();
+}
+
+class _UserProfilePageState extends State<UserProfilePage> {
+  User? _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final lastLoggedInUserEmail = prefs.getString('lastLoggedInUser');
+
+    if (lastLoggedInUserEmail != null) {
+      final userString = prefs.getString(lastLoggedInUserEmail);
+      if (userString != null) {
+        final Map<String, dynamic> userMap =
+        jsonDecode(userString) as Map<String, dynamic>;
+        setState(() {
+          _user = User.fromJson(userMap);
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('User Profile'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.edit),
-            onPressed: () {},
-          ),
-        ],
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
+          child: _user != null
+              ? Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: ResponsiveConfig.avatarRadius(context),
-                backgroundImage: const AssetImage('assets/place_holder.jpg'),
+                backgroundImage:
+                const AssetImage('assets/place_holder.jpg'),
               ),
               SizedBox(height: ResponsiveConfig.spacing(context)),
               Text(
-                'Name: John Doe',
+                'Name: ${_user!.name}',
                 style: TextStyle(
                   fontSize: ResponsiveConfig.fontSizeName(context),
                   fontWeight: FontWeight.bold,
@@ -38,13 +66,15 @@ class UserProfilePage extends StatelessWidget {
               ),
               SizedBox(height: ResponsiveConfig.spacing(context) / 2),
               Text(
-                'Email: johndoe@example.com',
+                'Email: ${_user!.email}',
                 style: TextStyle(
-                    fontSize: ResponsiveConfig.fontSizeEmail(context),),
+                  fontSize: ResponsiveConfig.fontSizeEmail(context),
+                ),
               ),
               SizedBox(height: ResponsiveConfig.spacing(context)),
             ],
-          ),
+          )
+              : const Column(),
         ),
       ),
     );
